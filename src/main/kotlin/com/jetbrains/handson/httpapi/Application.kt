6bed763e5df0ce0ffd58d19e9390097991d986cd
cuongtd <trancuong200495@gmail.com>
2021-06-06 22:58:32 +0700

    fucking simple api

--- src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/httpapi/Application.kt
@@ -1,10 +1,20 @@
 package com.jetbrains.handson.httpapi
 
-import io.ktor.application.Application
+import com.jetbrains.handson.httpapi.routes.registerCustomerRoutes
+import com.jetbrains.handson.httpapi.routes.registerOrderRoutes
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.serialization.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
-
+    install(ContentNegotiation) {
+        json()
+    }
+    registerCustomerRoutes()
+    registerOrderRoutes()
 }
